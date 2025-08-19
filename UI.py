8853c810dc9b158df443f61e2c0d7f6e481c5dfe
crystallineangel <crystallineangel383@gmail.com>
2025-08-19 16:15:55 +0800

    Added warning message for fewer recs

--- UI.py
+++ UI.py
@@ -74,6 +74,9 @@ with col3:
             st.divider()
             st.write(f"Hi, here are your recommendations!")
 
+            if rec.not_enough_recs = True:
+                st.write(f"Only found {len(rec.final_recs)} books instead of {len(rec.rec_num)} in our current library!")
+
             for idx, row in recommended_books.iterrows():
                 name = str(row['title'])
                 author = str(row['author'])
